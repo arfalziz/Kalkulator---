90864a0f716878496ee8654f2f6300db6c9cfbc3
arfalziz <arifalazis145@gmail.com>
2024-09-04 14:09:09 +0700

    kalkulator.py
    tambah kurang kalli bagi

--- kalkulator.py
+++ kalkulator.py
@@ -1,1 +1,33 @@
+def kalkulator():
+    while True :
+        print("pilih operasi:")
+        print ("1. tambah")
+        print ("2. kurang")
+        print ("3. kali")
+        print ("4. bagi")
+        print ("5. keluar")
+
+       
+
+        operasi = input ("masukan nomor pilihan (1,2,3,4):")
+        bilangan1 = int(input("masukan bilangan 1:"))
+        bilangan2 = int(input("masukan bilangan 2:"))
+        
+
+        if operasi == '1':
+                hasil = bilangan1 + bilangan2
+                print(f"{hasil}: {bilangan1} + {bilangan2} == {hasil}")
+        elif operasi =='2':
+                hasil = bilangan1 - bilangan2
+                print(f"{hasil}: {bilangan1} - {bilangan2}== {hasil}")
+        elif operasi == '3':
+                hasil = bilangan1 * bilangan2
+                print(f"{hasil}: {bilangan1} * {bilangan2} == {hasil}")
+        elif operasi == '4':
+                hasil = bilangan1 / bilangan2
+                print(f"{hasil}: {bilangan1} / {bilangan2} == {hasil}")
+
+        else :
+                print("pilihan tidak valid")
+kalkulator()
 
